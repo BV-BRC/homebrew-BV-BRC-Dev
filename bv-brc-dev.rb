934d02a811633948cf6b038c3c131fc8be4a576e
Robert Olson <olson@mcs.anl.gov>
2025-05-16 15:01:06 -0500

    Remove bowtie2 dependency

--- bv-brc-dev.rb
+++ bv-brc-dev.rb
@@ -6,7 +6,6 @@ class BvBrcDev < Formula
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   version "1.0"
 
-  depends_on "bowtie2"
   depends_on "perl"
 
   resource "Hash::MultiValue" do
